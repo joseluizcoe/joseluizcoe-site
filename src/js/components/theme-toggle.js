d39177533d10205dd5b7562ac57467dabb211d9a
José Luiz Coe <joseluizcoe@gmail.com>
2022-10-11 10:26:42 -0300

    Removing dark mode

--- src/js/components/theme-toggle.js
+++ src/js/components/theme-toggle.js
@@ -65,9 +65,8 @@ class ThemeToggle extends HTMLElement {
   }
 
   render() {
-    return;
     this.innerHTML = html`
-      <div class="[ theme-toggle ] [ md:ta-right gap-top-500 ]">
+      <div class="[ theme-toggle ] [ visually-hidden ] [ md:ta-right gap-top-500 ]">
         <div role="status" class="[ visually-hidden ][ js-mode-status ]"></div>
         <button class="[ button ] [ font-base text-base weight-bold ] [ js-mode-toggle ]">
           Dark theme
